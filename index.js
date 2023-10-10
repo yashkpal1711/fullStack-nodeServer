49d3ebaba328fdad6f9ce7d9d9a2835329747ad2
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-10 14:32:18 +0530

    Created Update user API

--- index.js
+++ index.js
@@ -7,20 +7,13 @@ const cors = require("cors");
 // Body Parser
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+// importing the Model from the models folder
+const User = require("./models/Users");
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/demo");
   console.log("db Connected");
 }
-// Creating a Schema- which filter and validates the incoming data
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-//  Creating a Model, which can be considered as a Class and we can make objects of it
-//  it'll be helpful bcoz out data will be stored as a JSON
-const User = mongoose.model("User", userSchema);
 
 const server = express();
 // We use middleware Like this
@@ -44,27 +37,44 @@ server.get("/demo", async (req, res) => {
   const docs = await User.find({});
   res.json(docs);
 });
+server.get("/demo/getUser/:id", async (req, res) => {
+  const idToUpdate = req.params.id;
+  const docs = await User.findById({ _id: idToUpdate });
+  res.json(docs);
+});
+
+server.put("/demo/updateUser/:id", async (req, res) => {
+  const idToUpdate = req.params.id;
+  const docs = await User.findByIdAndUpdate(
+    { _id: idToUpdate },
+    {
+      username: req.body.username,
+      password: req.body.password,
+    }
+  ); 
+  res.json(docs);
+});
 
 server.delete("/demo/:id", async (req, res) => {
-    try {
-      const idToDelete = req.params.id;
-  
-      // Use User.deleteOne() to delete a user by username
-      const result = await User.deleteOne({ _id: idToDelete });
-  
-      if (result.deletedCount === 1) {
-        // The user was deleted successfully
-        res.status(200).json({ message: "User deleted successfully." });
-      } else {
-        // User not found
-        res.status(404).json({ message: "User not found." });
-      }
-    } catch (error) {
-      // Handle errors
-      console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
+  try {
+    const idToDelete = req.params.id;
+
+    // Use User.deleteOne() to delete a user by username
+    const result = await User.deleteOne({ _id: idToDelete });
+
+    if (result.deletedCount === 1) {
+      // The user was deleted successfully
+      res.status(200).json({ message: "User deleted successfully." });
+    } else {
+      // User not found
+      res.status(404).json({ message: "User not found." });
     }
-  });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 server.listen(8080, () => {
   console.log("server started at 8080");
